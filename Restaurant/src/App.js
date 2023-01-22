58bc8edc1093691d76d44e3d7af3d7dfe7b1fe94
Suriyaprakash-Github <yoursgump@gmail.com>
2023-01-23 00:11:46 +0530

    Cart Modal Close

--- Restaurant/src/App.js
+++ Restaurant/src/App.js
@@ -1,15 +1,24 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
-
 import Header from "./components/Layout/Header";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCarthandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <Fragment>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCarthandler} />
       <main>
         <Meals />
       </main>
