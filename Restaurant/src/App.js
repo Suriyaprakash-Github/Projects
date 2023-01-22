f6116a143fd03f6175be2eb828ef1f5891d6d428
Suriyaprakash-Github <yoursgump@gmail.com>
2023-01-22 23:30:00 +0530

    Cart Modal

--- Restaurant/src/App.js
+++ Restaurant/src/App.js
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
